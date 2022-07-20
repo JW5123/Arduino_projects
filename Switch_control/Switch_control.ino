cb6f524e032ad2e555a9a4f6c49429a958a52ed2
JW5123 <Jay2002714@gmail.com>
2022-07-21 01:38:27 +0800

    add new file

---
+++ Switch_control/Switch_control.ino
@@ -0,0 +1,19 @@
+//開關及控制LED亮與暗
+const int BUTTON_Pin = 2;
+const int LED_Pin = 13;
+const int ButtonDelay = 100;
+int LED_State = LOW;
+void setup(){
+    pinMode(BUTTON_Pin, INPUT_PULLUP);
+    pinMode(LED_Pin, OUTPUT);
+}
+void loop(){
+    int val = digitalRead(BUTTON_Pin);
+    if(val == LOW){
+        delay(ButtonDelay);
+        while(digitalRead(BUTTON_Pin) == LOW)
+            //;                                   //等待放開按鍵
+        LED_State = !LED_State;
+        digitalWrite(LED_Pin, LED_State);
+    }
+}
